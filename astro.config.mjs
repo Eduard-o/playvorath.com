522f46a02d5fcd10ad37157315cacf462042cc4d
Eduardo Abreu <41760191+Eduard-o@users.noreply.github.com>
2023-08-21 21:17:08 -1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eduard-o.github.io',
-  base: '/vorath',
+  base: '/playvorath.com',
 });
